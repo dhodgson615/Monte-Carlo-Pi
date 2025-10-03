e20b53026c111e574e3bf1b4e9db5756e649ba91
dhodgson615 <dhodgson0615@gmail.com>
2025-10-03 15:03:43 -0400

    Refactor

--- local_monte.py
+++ local_monte.py
@@ -76,8 +76,6 @@ def monte_carlo_pi() -> None:
     fig, ax, circle = setup_plot()
 
     while True:
-        x: float
-        y: float
         x, y = random.uniform(-1, 1), random.uniform(-1, 1)
         total += 1
         if x**2 + y**2 <= 1:
